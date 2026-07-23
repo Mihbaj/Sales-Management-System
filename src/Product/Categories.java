49ac44a42a3f56b6c4bf358b3055e9e026bb1609
Mihbaj <mihbaj10@gmail.com>
2026-07-24 02:03:00 +0530

    Creating Categories.java file in in Product package

---
+++ src/Product/Categories.java
@@ -0,0 +1,18 @@
+public class Categories{
+    String[] categories = new String[10];
+    
+    //categories[1]= something , add like 10 Categories
+
+    public void displayCategories(){
+        for(int i=0;i<categories.length;i++){
+            System.out.println("Category "+i+": "+categories[i]);
+
+        }
+    }
+    // must add methods to change the categories;
+
+
+
+
+
+}
